38609e9f4f35205767dc44c8dcad4ee5874b790f
bahag-banasiks <steffen.banasik@bahag.com>
2025-10-08 07:55:02 +0200

    push changes and corrections to v2 service

--- cloud-run.tf
+++ cloud-run.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_run_service_v2_service" "default" {
+resource "google_cloud_run_v2_service" "default" {
   name     = var.service_name
   location = var.region
   ingress  = var.ingress
@@ -6,6 +6,17 @@ resource "google_cloud_run_service_v2_service" "default" {
   template {
     containers {
       image = var.container_image
+
+      ports {
+        container_port = var.container_port
+      }
+
+      resources {
+        limits = {
+          cpu    = var.cpu
+          memory = var.memory
+        }
+      }
 
       dynamic "env" {
         for_each = var.environment_variables
@@ -16,31 +27,22 @@ resource "google_cloud_run_service_v2_service" "default" {
       }
     }
 
-    resources {
-      limits = {
-        cpu    = var.cpu
-        memory = var.memory
-      }
+    scaling {
+      max_instance_count = var.max_instance_count
+      min_instance_count = var.min_instance_count
     }
+  }
 
-    ports {
-      container_port = var.container_port
-    }
-
-    max_instance_count = var.max_instance_count
-    min_instance_count = var.min_instance_count
-
-    traffic {
-      type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-      percent = 100
-    }
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
   }
 }
 
-resource "google_cloud_run_service_v2_iam_binding" "public" {
+resource "google_cloud_run_v2_service_iam_binding" "public" {
   count    = var.make_public ? 1 : 0
-  name     = google_cloud_run_service_v2_service.default.name
-  location = google_cloud_run_service_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+  location = google_cloud_run_v2_service.default.location
   role     = "roles/run.invoker"
   members  = ["allUsers"]
 }
